053087f80057a895bd97d89af6ab6bb361eadecc
Andrea Turchet <andre.turky@gmail.com>
2022-05-11 16:22:29 +0200

    add partition

--- quickselect.c
+++ quickselect.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
-
+#include <stdlib.h>
+#define MAX_LINE_SIZE 1000   // maximum size of a line of input
 
 double duration(struct timespec start, struct timespec end) {
     return end.tv_sec - start.tv_sec
@@ -16,9 +17,47 @@ double getResolution(){
     return duration(start, end);
 }
 
+int scanArray(int *a) {
+    // scan line of text
+    char line[MAX_LINE_SIZE];
+    scanf("%[^\n]", line);
+
+    // convert text into array
+    int size = 0, offset = 0, numFilled, n;
+    do {
+        numFilled = sscanf(line + offset, "%d%n", &(a[size]), &n);
+        if (numFilled > 0) {
+            size++;
+            offset += n;
+        }
+    } while (numFilled > 0);
+
+    return size;
+}
+
+void swap(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+    
+int partition(int arr[], int l, int r){
+    int x = arr[r];
+    int i = l;
+    for(int j=l;j<r;j++){
+        if(arr[j]<=x){
+            swap(arr[i],arr[j]);
+            i++;
+        }
+    }
+    swap(arr[i],arr[r]);
+    return i;
+}
 int quickSelect(list, left, right, k){
-    if (left == right)   return list[right]
-    int pivotIndex = partition(list, left, right,pivotIndex);
+    if (left == right){
+        return list[right];
+    }   
+    int pivotIndex = partition(list, left, right);
     if (k == pivotIndex)
         return list[k];
     else if (k < pivotIndex)
@@ -27,9 +66,11 @@ int quickSelect(list, left, right, k){
         left = pivotIndex + 1;
 }
 
-int main()
-{
-    printf("Hello World");
-
+int main(int argc, const char * argv[]){
+    int a[1000];
+    int n=scanArray(a);
+    int k=..;
+    int qs = quickSelect(a,0,n-1,k);
+    printf("%d ", qs);
     return 0;
 }
