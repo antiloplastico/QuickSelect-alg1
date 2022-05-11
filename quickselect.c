a961e977f8494810e7a11db5024e37d78676dc1a
Rikydc01 <49121547+Rikydc01@users.noreply.github.com>
2022-05-11 15:39:25 +0200

    Created quickselect.c

---
+++ quickselect.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+
+
+double duration(struct timespec start, struct timespec end) {
+    return end.tv_sec - start.tv_sec
+         + ((end.tv_nsec - start.tv_nsec ) / (double) 1000000000.0);
+}
+
+double getResolution(){
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    do {
+        clock_gettime(CLOCK_MONOTONIC, &end);
+    } while (duration(start, end) == 0.0);
+    return duration(start, end);
+}
+
+
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
